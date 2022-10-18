1c2cb195c7f17032e9624c97549103aeb2684892
Ben Dean-Kawamura <bdk@mozilla.com>
2022-10-18 13:50:49 -0400

    Make `uniffi` the one crate that consumers need to depend on
    Make `uniffi` re-export the functionality from `uniffi_build`,
    `uniffi_bindgen`, and `uniffi_macros`.  This means it can be consumers
    only need to depend on uniffi.  This slightly simplifies the process,
    but the real reason is because it changes the versioning system.
    
    Updated `uniffi` to be version `1.0.0`.  The idea here is that this is
    the version number that consumers use, and we only need to bump it if we
    make some backward compatible change to how UDL files are processed.
    
    Updated the tutorial docs to describe the new system.  Also made them
    recommend the system for managing `uniffi-bindgen` that we're using in
    mozilla: create your own uniffi-bindgen binary so that your Cargo.toml
    file controls the version and you can keep it in sync with the `uniffi`
    used in your source code.

--- uniffi/src/lib.rs
+++ uniffi/src/lib.rs
@@ -2,8 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/// Reexport all items from uniffi_core
+pub use uniffi_build::generate_scaffolding;
+/// Reexport items from other uniffi creates
 pub use uniffi_core::*;
+pub use uniffi_macros::{build_foreign_language_testcases, include_scaffolding};
 
 #[cfg(test)]
 mod test {
@@ -12,4 +14,8 @@ mod test {
         let t = trybuild::TestCases::new();
         t.compile_fail("tests/ui/*.rs");
     }
+}
+
+pub fn uniffi_bindgen_main() {
+    uniffi_bindgen::run_main().unwrap();
 }
