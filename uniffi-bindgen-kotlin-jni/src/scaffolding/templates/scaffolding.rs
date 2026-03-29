7c9adaa286d5bebc829fe966b2ca818cb904e96c
Ben Dean-Kawamura <bdk@mozilla.com>
2026-03-29 10:13:28 -0400

    uniffi-bindgen-kotlin-jni: primitive types
    Pass ints, floats and strings using JNI. Added a section in `DESIGN.md`
    and some code in `ffi_types.rs` describing the plan to pass other types
    as primitives.
    
    Added the `ArgFfi` and `ReturnFfi` enums to prepare for other methods of
    passing values across the FFI.  Right now there's just one variant, but
    I'll be adding another soon.

--- uniffi-bindgen-kotlin-jni/src/scaffolding/templates/scaffolding.rs
+++ uniffi-bindgen-kotlin-jni/src/scaffolding/templates/scaffolding.rs
@@ -1,6 +1,8 @@
 // Skip clippy checks for the generate bindings
 #[allow(clippy::unused_unit)]
 #[allow(clippy::let_unit_value)]
+#[allow(clippy::type_complexity)]
+#[allow(unused)]
 mod uniffi_bindgen_kotlin_jni_scaffolding {
     use ::uniffi_bindgen_kotlin_jni_runtime as uniffi_jni;
     use ::uniffi_bindgen_kotlin_jni_runtime::uniffi;
