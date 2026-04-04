7b9a51888796a52470db5bfe9f88b47a0b484dda
Ben Dean-Kawamura <bdk@mozilla.com>
2026-04-04 19:38:29 -0400

    uniffi-bindgen-kotlin-jni: Errors
    I handled these by throwing an exception directly from Rust rather than
    using something like `RustCallStatus. I figured it was better to use the
    built-in JVM exception handling then a separate call status value.
    
    Created a `CallableResult` type that stores the return + throws types.
    This will be helpful going forward, since we can do things like find all
    unique return/throw combinations.
    
    Added support for flat error enums and fixed the naming code to replace
    `Error` with `Exception`.

--- uniffi-bindgen-kotlin-jni/src/pipeline/nodes.rs
+++ uniffi-bindgen-kotlin-jni/src/pipeline/nodes.rs
@@ -175,9 +175,7 @@ pub struct Callable {
     pub fully_qualified_name_rs: String,
     pub receiver: Option<Argument>,
     pub arguments: Vec<Argument>,
-    pub return_type: Option<TypeNode>,
-    pub throws_type: Option<TypeNode>,
-    pub return_ffi: ReturnFfi,
+    pub result: CallableResult,
 }
 
 #[derive(Debug, Clone, Node, MapNode)]
@@ -196,6 +194,13 @@ pub enum CallableKind {
         self_type: TypeNode,
         for_callback_interface: bool,
     },
+}
+
+#[derive(Debug, Clone, Node)]
+pub struct CallableResult {
+    pub return_type: Option<TypeNode>,
+    pub throws_type: Option<TypeNode>,
+    pub return_ffi: ReturnFfi,
 }
 
 #[derive(Debug, Clone, Node)]
