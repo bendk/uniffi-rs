43cb5d45990243267fb7ffdc5d5a9856955bc17b
Ben Dean-Kawamura <bdk@mozilla.com>
2026-05-22 08:56:43 -0400

    uniffi-bindgen-kotlin-jni: optionals
    If we can find a niche value to use, lower optionals to a primitive.
    Otherwise, lower them to the inner type's FFI type plus a boolean.
    No tests yet, since the option tests are tied to the vec/hashmap tests
    and I haven't implemented those yet.
    
    Now that we have optionals, these functions can return something like
    `ULong?` which causes the Kotlin name mangling to kick in. Add
    `@JvmName` annotations to scaffolding functions to avoid this.

--- uniffi-bindgen-kotlin-jni/src/pipeline/nodes.rs
+++ uniffi-bindgen-kotlin-jni/src/pipeline/nodes.rs
@@ -34,6 +34,7 @@ pub struct Package {
 pub enum TypeDefinition {
     Record(Record),
     Enum(Enum),
+    Optional(OptionalType),
 }
 
 #[derive(Debug, Clone, Node, MapNode)]
@@ -140,6 +141,13 @@ pub struct Argument {
     pub ty: TypeNode,
     pub optional: bool,
     pub ffi_args: Vec<FfiArgument>,
+}
+
+#[derive(Debug, Clone, Node, MapNode)]
+#[map_node(from(general::OptionalType))]
+pub struct OptionalType {
+    pub inner: TypeNode,
+    pub self_type: TypeNode,
 }
 
 /// Wrap `Type` so that we can add extra fields that are set for all variants.
