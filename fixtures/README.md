ed9e4a0ecd9239c4d89191fd4f8648a1f67bd720
Ben Dean-Kawamura <bdk@mozilla.com>
2026-09-16 09:16:37 -0400

    Removing some redundant test fixtures
    Removing the callbacks and enums test fixtures.
    Updated the corresponding bindgen-tests with missing features that
    claude found.

---
+++ fixtures/README.md
@@ -0,0 +1,3 @@
+Miscellaneous UniFFI fixtures
+
+Note: most fixtures have been migrated to the `../bindgen-tests` directory.
--- fixtures/error-types/tests/test_generated_bindings.rs
+++
@@ -1,6 +0,0 @@
-uniffi::build_foreign_language_testcases!(
-    "tests/bindings/test.py",
-    "tests/bindings/test.kts",
-    "tests/bindings/test.swift",
-    "tests/bindings/test.rb",
-);
